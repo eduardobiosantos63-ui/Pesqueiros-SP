32f2ec1089504e537858bede7d0da6da3631c78a
eduardobiosantos63-ui <eduardo.bio.santos63@gmail.com>
2025-11-25 18:08:54 -0300

    FIX: Corrigido 404/401 de manifest e assets (configuração Vite).

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // --- ADICIONADO PARA CORRIGIR O PATH DE ASSETS NO VERCEL ---
+  base: '/', 
+  publicDir: 'public', 
+  // --- FIM ADICIONADO ---
   plugins: [react()],
 })
